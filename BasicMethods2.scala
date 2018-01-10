8d010a34f35a6a1143c8d6e9d89e121c6f0a0800
pantera-rapida <35178471+pantera-rapida@users.noreply.github.com>
2018-01-09 20:45:19 -0500

    Add files via upload

--- BasicMethods2.scala
+++ BasicMethods2.scala
@@ -1,5 +1,14 @@
+
+// return type can be inferred; x and 7 are Int will return an Int, don't need to specify a return type
 def add(x:Int, y:Int) = x + y
 
 println(add(6, 7))
+
+// method takes an Int type but returns a String, all 3 stmts are strings
+def numberStatus(a:Int) = 
+	if (a < 10) "Less than 10"
+	else if (a > 10) "Greater than 10"
+	else "It is 10!"
+println(numberStatus(10))
 
 
